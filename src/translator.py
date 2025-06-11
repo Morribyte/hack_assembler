a5af3092e3c6c1a1679debe066b83d6fda8e25f4
Starstreak <Starstreak1@proton.me>
2025-06-11 12:42:21 -0700

    - Created translator_test.py and associated main file. Added test for object and it passes.
    - Created first test to convert jump bits.
    
    - Test series making sure all the bit fields match

--- src/translator.py
+++ src/translator.py
@@ -8,10 +8,19 @@ class Translator:
     Represents a translator object
     """
     def __init__(self):
-        pass
+        self.jump: dict = {
+            "null": "000",
+            "JGT": "001",
+            "JEQ": "010",
+            "JGE": "011",
+            "JLT": "100",
+            "JNE": "101",
+            "JLE": "110",
+            "JMP": "111",
+        }
 
     def convert_jump(self, command: str) -> str:
         """
         Takes in a "C" command and returns a set of three bits.
         """
-        return "111"
+        return self.jump.get(command)
--- test/translator_test.py
+++ test/translator_test.py
@@ -5,6 +5,17 @@ from src.translator import Translator
 
 import pytest
 
+jump_dict: dict = {
+            "null": "000",
+            "JGT": "001",
+            "JEQ": "010",
+            "JGE": "011",
+            "JLT": "100",
+            "JNE": "101",
+            "JLE": "110",
+            "JMP": "111",
+        }
+
 @pytest.fixture
 def setup_resources():
     """
@@ -30,5 +41,11 @@ def test_convert_jump(setup_resources):
     Test that we can convert the jump command to bits
     """
     translator = setup_resources["translator"]
-    value = translator.convert_jump("111")
+    value = translator.convert_jump("JMP")
+    print(value)
     assert value == "111"
+
+
+@pytest.mark.parametrize("mnemonic, expected_binary", jump_dict.items())
+def test_jump_lookup(mnemonic, expected_binary):
+    assert jump_dict.get(mnemonic) == expected_binary
